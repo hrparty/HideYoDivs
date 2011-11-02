4f0d6323073c194988739b4e4dfbc8c56b8098a5
Hlynur Tryggvason (hrparty) <h@herraparty.com>
2011-11-02 21:56:48 +0000

    Added support for iOS touch events.

--- js/plugins/jquery.hideyodivs.js
+++ js/plugins/jquery.hideyodivs.js
@@ -24,7 +24,7 @@
 
 			timer = setTimeout(methods.hide, settings.timeout);
 
-    		$(window).bind('mousemove.hideYoDivs', function() {
+    		$(window).bind('mousemove.hideYoDivs touchmove touchstart', function() {
 				methods.show();
 			});
 			
