6627ad6e870c87e3b5112c6b4961207e63d46256
Hlynur Tryggvason (hrparty) <h@herraparty.com>
2012-03-07 20:55:12 +0000

    Fixing indentation problems
    Got spaces and tabs mixed up, changed to use only spaces.

--- js/plugins/jquery.hideyodivs.js
+++ js/plugins/jquery.hideyodivs.js
@@ -7,63 +7,63 @@
  */
 (function($){
 
-	var $t,
-		timer,
-		settings = {
-	    	timeout: 2000,
+    var $t,
+        timer,
+        settings = {
+            timeout: 2000,
             fadeInSpeed: 400,
             fadeOutSpeed: 400
-	    };
+        };
 
-	var methods = {
-    	init: function(options) {
-    		
-    		if ( options ) { 
-				$.extend( settings, options );
-			}
+    var methods = {
+        init: function(options) {
+            
+            if ( options ) { 
+                $.extend( settings, options );
+            }
 
-			timer = setTimeout(methods.hide, settings.timeout);
+            timer = setTimeout(methods.hide, settings.timeout);
 
-    		$(window).bind('mousemove.hideYoDivs touchmove.hideYoDivs touchstart.hideYoDivs', function() {
-				methods.show();
-			});
-			
-			return $t;
-    	},
-    	destroy: function() {
-    		
+            $(window).bind('mousemove.hideYoDivs touchmove.hideYoDivs touchstart.hideYoDivs', function() {
+                methods.show();
+            });
+            
+            return $t;
+        },
+        destroy: function() {
+            
             clearTimeout(timer);
-    		$(window).unbind('.hideYoDivs');
+            $(window).unbind('.hideYoDivs');
             $t.show();
 
-    		return $t;
-    	},
-    	show: function() {
-    		
-    		return $t.each(function() {
+            return $t;
+        },
+        show: function() {
+            
+            return $t.each(function() {
                 clearTimeout(timer);
-    			$(this).fadeIn(settings.fadeInSpeed);
-    			timer = setTimeout(methods.hide, settings.timeout)
-    		});
-    	},
-    	hide: function() {
-    		
-    		return $t.each(function() {
-    			$(this).fadeOut(settings.fadeOutSpeed);
-    		});
-    	}
+                $(this).fadeIn(settings.fadeInSpeed);
+                timer = setTimeout(methods.hide, settings.timeout)
+            });
+        },
+        hide: function() {
+            
+            return $t.each(function() {
+                $(this).fadeOut(settings.fadeOutSpeed);
+            });
+        }
     };
 
-	$.fn.hideYoDivs = function( method ) {  
+    $.fn.hideYoDivs = function( method ) {  
 
-		$t = this;
+        $t = this;
 
-		if ( methods[method] ) {
-	    	return methods[method].apply( this, Array.prototype.slice.call( arguments, 1 ));
-	    } else if ( typeof method === 'object' || ! method ) {
-	    	return methods.init.apply( this, arguments );
-	    } else {
-	    	$.error( 'Method ' +  method + ' does not exist on jQuery.hideYoDivs' );
-	    }
-  	};
+        if ( methods[method] ) {
+            return methods[method].apply( this, Array.prototype.slice.call( arguments, 1 ));
+        } else if ( typeof method === 'object' || ! method ) {
+            return methods.init.apply( this, arguments );
+        } else {
+            $.error( 'Method ' +  method + ' does not exist on jQuery.hideYoDivs' );
+        }
+    };
 })(jQuery);
